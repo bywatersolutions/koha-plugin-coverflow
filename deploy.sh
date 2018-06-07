a0291600a45ebe8489f846dda45f89cc00ca19ef
Kyle M Hall <kyle@bywatetsolutions.com>
2018-06-07 06:22:57 -0400

    Update deploy.sh to use 'ci skip'

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-if git log -1 --pretty=oneline | grep -v 'Version auto-incremented'
+if echo $TRAVIS_BRANCH | grep master
 then
-  if echo $TRAVIS_BRANCH | grep master
-  then
-    echo "Building release"
-    node increment_version.js
-    git commit -a -m "Version auto-incremented  - $TRAVIS_JOB_NUMBER"
-    gulp build
-    gulp release
-    git remote add github https://$GITHUB_TOKEN@github.com/bywatersolutions/koha-plugin-coverflow
-    git fetch --all
-    git push github HEAD:master
-  fi
-else
-  echo "No release needing."
+  echo "Building release"
+  node increment_version.js
+  git commit -a -m "Version auto-incremented  - $TRAVIS_JOB_NUMBER [skip ci]"
+  gulp build
+  gulp release
+  git remote add github https://$GITHUB_TOKEN@github.com/bywatersolutions/koha-plugin-coverflow
+  git fetch --all
+  git push github HEAD:master
 fi
